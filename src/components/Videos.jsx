f851aee5245065ba1b851d380b025c03c2fa286a
SkyDe <sky_de1991@outlook.com>
2023-02-28 03:47:45 -0800

    video page Done

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,12 @@
 import { Box, Stack } from "@mui/system";
 import { VideoCard, ChannelCard} from "./";
 
-const Videos = ({ videos }) => {
-  console.log(videos);
+const Videos = ({ videos, direction }) => {
+
+  if(!videos?.length) return "Loading...";
+  
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={ direction || "row" } flexWrap="wrap" justifyContent="start" gap={2}>
       {videos && videos.map((item,index) => (
         <Box key={index}>
           {item.id.channelId && <ChannelCard channelDetail={item} bd="solid"/>}
